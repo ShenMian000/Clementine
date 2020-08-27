641fd617499e446e9c5c25eb171e47f51c983073
ShenMian <sms_school@outlook.com>
2020-08-27 20:57:15 +0800

    update

--- clem/coord.hpp
+++ clem/coord.hpp
@@ -8,20 +8,20 @@
 
 struct Coord
 {
-	Coord(double x = 0, double y = 0)
+	Coord(float x = 0, float y = 0)
 			: x(x), y(y)
 	{
 	}
 
 	// 返回两点间直线距离
-	double distanceTo(const Coord& coord)
+	float distanceTo(const Coord& coord)
 	{
 		Coord dec = {fabs(x - coord.x), fabs(y - coord.y)};
 		return sqrt(dec.x * dec.x + dec.y * dec.y);
 	}
 
-	double x;
-	double y;
+	float x;
+	float y;
 };
 
 #endif // CLEM_COORD_HPP_
