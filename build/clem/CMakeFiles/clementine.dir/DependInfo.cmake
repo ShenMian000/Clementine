fa43c492685caf59ee7f97949986efad43be103e
ShenMian <sms_school@outlook.com>
2020-08-28 22:34:33 +0800

    update

---
+++ build/clem/CMakeFiles/clementine.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sms/Clementine/clem/attribute.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/attribute.cpp.o"
+  "/home/sms/Clementine/clem/camera.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/camera.cpp.o"
+  "/home/sms/Clementine/clem/game_object.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/game_object.cpp.o"
+  "/home/sms/Clementine/clem/input_component.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/input_component.cpp.o"
+  "/home/sms/Clementine/clem/linux_renderer.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/linux_renderer.cpp.o"
+  "/home/sms/Clementine/clem/scene.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/scene.cpp.o"
+  "/home/sms/Clementine/clem/terminal.cpp" "/home/sms/Clementine/build/clem/CMakeFiles/clementine.dir/terminal.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../D:/Program Files/Program/boost_1_73_0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
