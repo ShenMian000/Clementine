c35d7e4f4671b5c63f8d6ac966bbd257bd77fa48
ShenMian <sms_school@outlook.com>
2020-08-30 01:25:12 +0800

    update

--- examples/balls/main.cpp
+++ examples/balls/main.cpp
@@ -7,10 +7,10 @@
 
 #ifdef OS_WIN
 #include <windows.h>
-#define SLEEP(ms) Sleep(ms)
+#define sleep(ms) Sleep(ms)
 #else
 #include <sys/wait.h>
-#define SLEEP(ms) sleep(ms * 1000)
+#define sleep(ms) usleep(ms * 1000)
 #endif
 
 using namespace std;
@@ -65,6 +65,6 @@ int main()
 		scene.update();
 		scene.render();
 
-		SLEEP(50);
+		sleep(50);
 	}
 }
